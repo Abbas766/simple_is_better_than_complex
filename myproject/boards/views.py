9784881500119c834acbdce0133f57fe29889523
Abbas Ali Cyclewala <blade766@yahoo.com>
2018-05-09 02:27:31 +0530

    adde some tests and new_topic view

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404, get_object_or_404
 from django.http import HttpResponse, Http404
 from boards.models import Board
 
@@ -9,6 +9,11 @@ def home(request):
 def board_topics(request, pk):
     try:
         board = Board.objects.get(pk=pk)
+        print(board.name)
     except Board.DoesNotExist:
         raise Http404
     return render(request, 'topics.html', {'board': board})
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board,pk=pk)
+    return render(request,'new_topic.html',{'board':board})

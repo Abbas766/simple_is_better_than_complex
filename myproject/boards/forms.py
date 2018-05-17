754ec8085e7714f102faf213eddcf58552c43671
Abbas Ali Cyclewala <blade766@yahoo.com>
2018-05-18 01:05:15 +0530

    Some more updates

--- myproject/boards/forms.py
+++ myproject/boards/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from .models import Topic
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(
-        attrs={'rows':5,'placeholder':'What is on your mind ?' }
-    ),
+    message = forms.CharField(widget=forms.Textarea(),
     max_length=4000,
     help_text = 'The max length of text is 4000.'
     )
